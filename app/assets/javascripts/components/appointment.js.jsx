42979e4c5b6297e6f120c31cb5f8d897d1fb6199
Barreira <barreira266@hotmail.com>
2018-07-29 19:48:54 -0300

    add dlç

---
+++ app/assets/javascripts/components/appointment.js.jsx
@@ -0,0 +1,10 @@
+var Appointment = createReactClass({
+
+  render: function() {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+});
+
---
+++ app/assets/javascripts/components/appointments.js.jsx
@@ -0,0 +1,11 @@
+var Appointments = createReactClass({
+
+  render: function() {
+    return (
+      <React.Fragment>
+        <h1>título aleatório</h1>
+      </React.Fragment>
+    );
+  }
+});
+
