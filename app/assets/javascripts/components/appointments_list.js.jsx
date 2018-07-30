3c9d5cb8b9bab91b4a93ad4477e90bdb2a63c827
Barreira <barreira266@hotmail.com>
2018-07-29 22:27:04 -0300

    componentes anestados deliciosamente

---
+++ app/assets/javascripts/components/appointments_list.js.jsx
@@ -0,0 +1,17 @@
+var AppointmentsList = createReactClass({
+
+  render: function() {
+    return (
+      <React.Fragment>
+        <div>
+          {this.props.appointments.map(function(appointment){
+              return(
+              <Appointment appointment={appointment} />
+              )      
+            })}
+        </div>
+      </React.Fragment>
+    );
+  }
+});
+
